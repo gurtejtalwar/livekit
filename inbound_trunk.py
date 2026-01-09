508c10f064f70778eda4ca069b73345a403ff6dc
gtbt <gurtejtalwar@gmail.com>
2026-01-09 13:03:17 +0530

    update trunk list

--- inbound_trunk.py
+++ inbound_trunk.py
@@ -4,23 +4,31 @@ from dotenv import load_dotenv
 from livekit import api
 
 load_dotenv(dotenv_path=".env",override=True)
-async def main():
-  livekit_api = api.LiveKitAPI()
-  # livekit_api = api.LiveKitAPI(url="ws://localhost:7880", api_key="devkey", api_secret="secret")
+# async def main():
+#   livekit_api = api.LiveKitAPI()
+#   # livekit_api = api.LiveKitAPI(url="ws://localhost:7880", api_key="devkey", api_secret="secret")
 
-  trunk = api.SIPInboundTrunkInfo(
-    name = "My trunk",
-    numbers = ["+12294713457"],
-    krisp_enabled = True,
-  )
+#   trunk = api.SIPInboundTrunkInfo(
+#     name = "My trunk",
+#     numbers = ["+12294713457"],
+#     krisp_enabled = True,
+#   )
 
-  request = api.CreateSIPInboundTrunkRequest(
-    trunk = trunk
-  )
+#   request = api.CreateSIPInboundTrunkRequest(
+#     trunk = trunk
+#   )
 
-  trunk = await livekit_api.sip.create_inbound_trunk(request)
-  print("created inbound trunk", trunk)
+#   trunk = await livekit_api.sip.create_inbound_trunk(request)
+#   print("created inbound trunk", trunk)
 
-  await livekit_api.aclose()
+#   await livekit_api.aclose()
 
-asyncio.run(main())
+# asyncio.run(main())
+
+async def run():
+    lkapi = api.LiveKitAPI()
+    try:
+        trunks = api.ListSIPDispatchRuleRequest()
+        print(trunks)
+    finally:
+        await lkapi.aclose()
