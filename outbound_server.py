1fda2755166c5d6f54c8d59b0cd3b001f201c573
gtbt <gurtejtalwar@gmail.com>
2026-01-12 13:43:42 +0530

    added: dotenv

--- outbound_server.py
+++ outbound_server.py
@@ -1,14 +1,18 @@
 import logging
+from dotenv import load_dotenv
 
 from livekit.agents import (
     cli
 )
 
 from app.services.agent.templates.outbound_agent.entrypoint import outbound_server # noqa: F401
+
+load_dotenv(override=True)
+
 if __name__ == "__main__":
     # Configure logging for better debugging
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        format='%(asc   time)s - %(name)s - %(levelname)s - %(message)s'
     )
     cli.run_app(outbound_server)
