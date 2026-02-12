b1cfb5fe807078fc201b28b80f9c06c1f1f34a27
gtbt <gurtejtalwar@gmail.com>
2026-02-12 15:24:40 +0530

    inc endpointing delay

--- app/services/agent/factory.py
+++ app/services/agent/factory.py
@@ -71,7 +71,7 @@ class InboundAgent(Agent):
             tools=tools,
             allow_interruptions=config.allow_interruptions,
             min_endpointing_delay=0.05,
-            max_endpointing_delay=0.6,
+            max_endpointing_delay=0.7,
         )
 
         self.config = config
