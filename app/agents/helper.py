2cfc6672e7b03976e6be971b65b1625f408295fc
gtbt <gurtejtalwar@gmail.com>
2026-03-11 15:43:30 +0530

    Revert "to revert"
    This reverts commit 18956e56ea20fd6db6db0602e85c1d7bdc35c6cc.

--- app/agents/helper.py
+++ app/agents/helper.py
@@ -76,7 +76,6 @@ async def load_agent_runtime_config(agent_id: str, user_data: UserData):
         else "Hello! How can I assist you today?"
     )
 
-    print(f"Workflow Config: {workflow_doc.workflow_config}") #TODO revert
     return agents.AgentConfig(
         user_id=str(user_data.user_id),
         agent_id=str(agent.id),
